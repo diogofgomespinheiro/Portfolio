d27968ab81fa8dd88695495aacbe84fd08f3647f
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-10-06 15:30:48 +0100

    Implementation of Home menu and mobile compability

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div style={{height: '1000px'}}> 
       <h1>About</h1>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+
+import Typewriter from "typewriter-effect";
+
+import "./Home.css";
+import linkedIn from "../../assets/linkedin.svg";
+import envelope from "../../assets/envelope.svg";
+import githubLogo from "../../assets/github-logo.svg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className="home">
+        <div className="content">
+          <div className="welcome-box">
+            {/* <span>Hello I´m</span> */}
+            <Typewriter
+              options={{
+                strings: [
+                  "Hello I´m",
+                  "Olá eu sou",
+                  "Bonjour je suis",
+                  "Hallo ich bin",
+                  "Hola yo soy"
+                ],
+                autoStart: true,
+                loop: true
+              }}
+            />
+          </div>
+          <div className="profile-information">
+            <h1>Diogo Pinheiro</h1>
+            <h2>FullStack Developer</h2>
+            <div className="social-media">
+              <a rel="noopener noreferrer" href="https://github.com/diogofgomespinheiro" target="_blank"><img src={githubLogo} alt="git" /></a>
+              <a rel="noopener noreferrer" href="https://www.linkedin.com/in/diogo-fgpinheiro/" target="_blank"><img src={linkedIn} alt="linkedIn" /></a>
+              <a rel="noopener noreferrer" href="https://github.com/diogofgomespinheiro" target="_blank"><img src={envelope} alt="email" /></a>
+            </div>
+          </div>
+        </div>
+        <div className="profile-picture"></div>
+      </section>
+      <div className="divider">
+        <h2>
+          "Power comes not from knowledge kept but from knowledge shared."
+        </h2>
+        <h2> - Bill gates</h2>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
