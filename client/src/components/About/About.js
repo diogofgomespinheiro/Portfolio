1d01122991d308f72ce12b6d410e3b08693d83c6
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-08 02:25:37 +0000

    add resume download

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React from "react";
+import pdf from "../../assets/resume.pdf";
 
-import './About.css';
+import "./About.css";
 
-import aboutImage from '../../assets/about-image.svg';
+import aboutImage from "../../assets/about-image.svg";
 
 const About = () => {
   return (
-    <section id="about-section" className="about"> 
-      <img src={aboutImage} alt="person"/>
+    <section id="about-section" className="about">
+      <img src={aboutImage} alt="person" />
       <div className="content">
         <h1>About Me</h1>
-        <p>Full Stack Developer with flair for creating elegant solutions in the least amount of time. Looking for growth opportunities to try new technologies and grow my technical skill set in a team environment.</p>
-        <p>Keys: Hard Worker, Team Player, Commitement, Ambition, Respect, Autodidact </p>
-        <button className="btn">Download CV</button>
+        <p>
+          Full Stack Developer with flair for creating elegant solutions in the
+          least amount of time. Looking for growth opportunities to try new
+          technologies and grow my technical skill set in a team environment.
+        </p>
+        <p>
+          Keys: Hard Worker, Team Player, Commitement, Ambition, Respect,
+          Autodidact{" "}
+        </p>
+        <button
+          className="btn"
+          onClick={() =>
+            window.open(pdf, "_blank")
+          }
+        >
+          Download CV
+        </button>
       </div>
     </section>
   );
-}
+};
 
 export default About;
